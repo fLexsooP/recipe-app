278faa81510386215a7f48719008c36dd062970f
Chence Shi <ascc594557067@gmail.com>
2023-04-14 02:33:25 -0700

    add server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
+require('dotenv').config();
+
 let app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const port = process.env.PORT || '8080';
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Server is running on :${port}`));
+
+app.listen(port, () => console.log(`Server is running on :${port}`));
